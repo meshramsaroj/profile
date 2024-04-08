31ea103e57bcf5fadd4f051e0737dd44c02f56d4
saroj-meshram-amla <saroj.meshram@amla.io>
2024-04-09 01:16:57 +0530

    fix: added key

--- app/component/ExperianceDetails/ExperianceDetails.tsx
+++ app/component/ExperianceDetails/ExperianceDetails.tsx
@@ -5,7 +5,7 @@ import { useState } from "react"
 
 const ExperianceDetails = ({query}: any) => {
   return (
-   <div className="flex flex-col gap-3 px-4">{data[query]?.map((exp: any) => <ExperianceCard detail={exp} />)}</div>
+   <div className="flex flex-col gap-3 px-4">{data[query]?.map((exp: any) => <ExperianceCard detail={exp} key={exp.id} />)}</div>
   )
 }
 
