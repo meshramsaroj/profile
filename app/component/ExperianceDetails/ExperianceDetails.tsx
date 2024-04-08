a7a6177aaa51255863ab9399ab06a22f7ccf3f4f
saroj-meshram-amla <saroj.meshram@amla.io>
2024-04-09 01:13:48 +0530

    fix: added key

--- app/component/ExperianceDetails/ExperianceDetails.tsx
+++ app/component/ExperianceDetails/ExperianceDetails.tsx
@@ -20,14 +20,14 @@ const ExperianceCard = ({detail}: any)=>{
                 <span>{!show? <AngleDown /> : <AngleUp />}</span>
                 </div>
             </div>
-            {show && <section className="accordian-content bg-white rounded-b-md p-2 grid grid-cols-3 gap-2 py-3">
-                {detail?.projects?.map(project => <div className="shadow-sm border-2 rounded-sm">
+            {show && <section className="accordian-content bg-white rounded-b-md p-2 grid grid-cols-3 gap-3 py-3">
+                {detail?.projects?.map((project:any) => <div key={project.projectName} className="shadow-sm border-2 rounded-sm">
                     <div className="border-b-2 p-2 flex justify-between align-middle flex-wrap">
                         <p><span className="font-medium">Project Name: </span>{project.projectName || "NA"}</p>
                         <Link href={project.projectUrl} className="cursor-pointer" target="_blank"><LinkOut /></Link>
                     </div>
                     <p></p>
-                    <div className="flex gap-2 flex-wrap px-2 py-3">{project.technologies.map(tech=> <span className="bg-slate-300 rounded-full px-2 hover:bg-slate-100">{tech}</span>)}</div>
+                    <div className="flex gap-2 flex-wrap px-2 py-3">{project.technologies.map((tech:any)=> <span key={tech} className="bg-slate-300 rounded-full px-2 hover:bg-slate-100">{tech}</span>)}</div>
                 </div>)}
             </section>}
         </div>
